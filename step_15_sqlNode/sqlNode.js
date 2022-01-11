9ca008006cb6864fe5262f7d0039336c6413753f
xutao <987427795@qq.com>
2022-01-11 22:06:18 +0800

    step 15

---
+++ step_15_sqlNode/sqlNode.js
@@ -0,0 +1,94 @@
+import { HtmlNode, HtmlNodeModel } from "@logicflow/core";
+
+class SqlNode extends HtmlNode {
+  setHtml(rootEl) {
+    rootEl.innerHTML = "";
+    const {
+      properties: { fields, tableName }
+    } = this.props.model;
+    rootEl.setAttribute("class", "table-container");
+    const container = document.createElement("div");
+    container.className = "table-node";
+    const tableNameElement = document.createElement("div");
+    tableNameElement.innerText = tableName;
+    tableNameElement.className = "table-name";
+    container.appendChild(tableNameElement);
+    const fragment = document.createDocumentFragment();
+    for (let i = 0; i < fields.length; i++) {
+      const item = fields[i];
+      const itemElement = document.createElement("div");
+      itemElement.className = "table-feild";
+      const itemKey = document.createElement("span");
+      itemKey.innerText = item.key;
+      const itemType = document.createElement("span");
+      itemType.innerText = item.type;
+      itemType.className = "feild-type";
+      itemElement.appendChild(itemKey);
+      itemElement.appendChild(itemType);
+      fragment.appendChild(itemElement);
+    }
+    container.appendChild(fragment);
+    rootEl.appendChild(container);
+  }
+}
+
+class SqlNodeModel extends HtmlNodeModel {
+  addField(item) {
+    this.properties.fields.push(item);
+    this.setAttributes();
+    // 为了保持节点顶部位置不变，在节点变化后，对节点进行一个位移,位移距离为添加高度的一半。
+    this.move(0, 24 / 2);
+  }
+  getOutlineStyle() {
+    const style = super.getOutlineStyle();
+    style.stroke = "none";
+    style.hover.stroke = "none";
+    return style;
+  }
+  setAttributes() {
+    this.width = 200;
+    const {
+      properties: { fields }
+    } = this;
+    this.height = 60 + fields.length * 24;
+    const circleOnlyAsTarget = {
+      message: "只允许从右边的锚点连出",
+      validate: (sourceNode, targetNode, sourceAnchor, targetAnchor) => {
+        return sourceAnchor.type === "right";
+      }
+    };
+    this.sourceRules.push(circleOnlyAsTarget);
+  }
+  getDetaultAnchor() {
+    const {
+      id,
+      x,
+      y,
+      width,
+      height,
+      properties: { fields }
+    } = this;
+    const anchors = [];
+    fields.forEach((feild, index) => {
+      anchors.push({
+        x: x - width / 2 + 10,
+        y: y - height / 2 + 60 + index * 24,
+        id: `${id}_1`,
+        type: "left"
+      });
+      anchors.push({
+        x: x + width / 2 - 10,
+        y: y - height / 2 + 60 + index * 24,
+        id: `${id}_1`,
+        type: "right"
+      });
+    });
+    return anchors;
+  }
+}
+
+export default {
+  type: "sql-node",
+  model: SqlNodeModel,
+  view: SqlNode
+};
